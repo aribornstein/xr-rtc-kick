5c282af118c177e31774493fbf1a9421ef9b56ed
aribornstein <x@x.com>
2025-03-22 01:36:55 +0200

    Refactor SDP utility functions; improve formatting and structure for better readability

--- js/utils.js
+++ js/utils.js
@@ -5,29 +5,31 @@ export function compressSDP(desc) {
     const json = JSON.stringify(desc);
     const compressed = pako.deflate(json, { to: 'string' });
     return encodeURIComponent(btoa(String.fromCharCode.apply(null, compressed)));
-  }
-  
-  // Decodes and decompresses an SDP description.
-  export function decompressSDP(str) {
+}
+
+// Decodes and decompresses an SDP description.
+export function decompressSDP(str) {
     try {
-      const decodedStr = decodeURIComponent(str);
-      return JSON.parse(pako.inflate(Uint8Array.from(atob(decodedStr), c => c.charCodeAt(0)), { to: 'string' }));
+        const decodedStr = decodeURIComponent(str);
+        return JSON.parse(pako.inflate(Uint8Array.from(atob(decodedStr), c => c.charCodeAt(0)), { to: 'string' }));
     } catch (error) {
-      console.error("Error decoding SDP:", error);
-      return null;
+        console.error("Error decoding SDP:", error);
+        return null;
     }
-  }
-  
-  // Updates the shareable URL input field.
-  export function updateShareableURL(param, value) {
-    if (!param === 'offer'){
+}
+
+// Updates the shareable URL input field.
+export function updateShareableURL(param, value) {
+    if (param === 'answer') {
         const url = new URL(window.location.href);
         const pathSegments = url.pathname.split('/');
         const rootDirectory = url.origin + pathSegments.slice(0, pathSegments.length - 1).join('/') + '/';
-        const shareableURL = rootDirectory + '?' + param + '=' + value;    
+        const shareableURL = rootDirectory + '?' + param + '=' + value;
+        document.getElementById('shareableUrl').value = shareableURL;
     }
-    document.getElementById('shareableUrl').value = value;
-  }
+    else {
+        document.getElementById('shareableUrl').value = value;
+    }
+}
 
 
-  
